11e7e0a50c115655b91d722a003f10f1b99a878e
zeninbiu <kincongviec@gmail.com>
2025-06-27 13:12:15 +1000

    update ui for rock paper scissors game

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-function getComputerChoice(){
-    // to get value 1,2 or 3
-    return Math.floor(Math.random() * 10) % 3 + 1
+let round_number = 1;
+let human_score = 0;
+let computer_score = 0;
 
+const choice_button = document.getElementsByClassName('choice--button');
+const counterDisplay = document.getElementById('counter')
+function playRound(event){
+    
+    round_number++;
+    counterDisplay.textContent = round_number;
+    alert(getResult(event.target.value))
+    if (round_number==6) {
+            alert(getSummary());
+            location.reload();
+            return
+        } 
 }
 
-function getHumanChoice(){
-    choice = prompt('Choose: rock, paper or scissor?').toLowerCase()
-    return choice === 'rock' ? 1 : choice == 'paper' ? 2 : choice == 'scissor' ? 3 : null
-}
+function getResult(human_choice_string){
+    computer_choice = Math.floor(Math.random() * 10) % 3 + 1
+    human_choice= human_choice_string== 'rock' ? 1 : human_choice_string== 'paper' ? 2 : human_choice_string== 'scissors' ? 3 : null
 
-function main(){
-    choice_computer = getComputerChoice()
-    choice_human = getHumanChoice()
-    choice_computer_text = choice_computer == 1 ? 'rock' : choice_computer == 2 ? 'paper': choice_computer == 3 ? 'scissor' : null
-    if (choice_human == null){
-        alert('Invalid choice')
-        return
+    if (human_choice == null){
+        return ('Invalid choice')
     }
+    else if(computer_choice == human_choice){
+        return('Tied')
+        
+    }
+    else if (computer_choice == 1 && human_choice == 3){
+        computer_score++;
+        return('You lose')
+    }
+    else if (computer_choice > human_choice){
+        computer_score++;
+        return('You lose')
 
-    alert(`Computer choice is ${choice_computer_text}`)
-
-    if(choice_computer == choice_human){
-        alert('Tied')
-        return
-    }
-    else if (choice_computer == 1 && choice_human == 3){
-        alert('You lose')
-        return
-    }
-    else if (choice_computer > choice_human){
-        alert('You lose')
-        return
     }
     else{
-        alert('You win')
-        return
+        human_score++;
+        return('You win')
     }
 }
 
-while(true){
-    main()
+function getSummary(){
+    return human_score < computer_score ? 'End game!! You lose a computer' : human_score > computer_score? 'End game !! You win a computer' : 'End game!! Tied game' 
 }
+
+Array.from(choice_button).forEach(button => {
+    button.addEventListener('click', playRound);
+});
